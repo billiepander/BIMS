58938666c7cccfc6e32c58eecfc68b0c9a401f1e
billiepander <billiepander@126.com>
2016-04-04 16:46:24 +0800

    add a mysql table about web class's budget and now you can check budget of a web and change the web class's budget

--- bridgeinformationmanagementsystem_wxpython/ChangeBetweenFrames.py
+++ bridgeinformationmanagementsystem_wxpython/ChangeBetweenFrames.py
@@ -149,6 +149,8 @@ class panel_searchResultShow(wx.Panel):
         sizer.Add(self.which2see, 0, wx.CENTER)
         self.SetSizer(sizer)
 
+
+
 class panel_MoneyTable(wx.Panel):
     def __init__(self, parent,result):
         self.result = result
@@ -193,6 +195,55 @@ class panel_MoneyTable(wx.Panel):
            db.rollback()
         db.close()
 
+class panel_RatifyWebMoney(wx.Panel):
+    def __init__(self, parent):
+        wx.Panel.__init__(self, parent=parent)
+        search = "select sum(bi.DeclareAmount) as 'ratified', bi.FatherWeb, wb.Ratified from bridgeinfo as bi JOIN WebBudget AS wb ON bi.FatherWeb=wb.WebName WHERE bi.DeclareProgress = '%s' GROUP BY bi.FatherWeb"%u"批准"
+        # search = "select sum(DeclareAmount) as 'ratified', FatherWeb from bridgeinfo WHERE DeclareProgress = '%s' GROUP BY FatherWeb"%u"批准"
+        self.result = exe(search)
+        self.Budget = {u"二环":10000,u'中国':3000}
+        sizer = wx.BoxSizer(wx.VERTICAL)
+        for i in range(len(self.result)):
+            self.griddetail = gridlib.Grid(self)
+            self.griddetail.CreateGrid(1,3)
+            # self.griddetail.SetRowAttr("colAtr")
+            self.griddetail.SetRowLabelValue(0,str(i+1))
+            self.griddetail.SetColLabelValue(0,"网络名".decode("gbk"))
+            self.griddetail.SetColLabelValue(1,"已用预算".decode("gbk"))
+            self.griddetail.SetColLabelValue(2,"总预算".decode("gbk"))
+            self.griddetail.SetCellValue(0,0,self.result[i][1])
+            self.griddetail.SetCellValue(0,1,str(self.result[i][0]))
+            print self.result[i][1]
+            print type(self.result[i][1])
+            self.griddetail.SetCellValue(0,2,self.result[i][2])
+            self.btn = wx.Button(self,-1,label="提交预算更改",name="第%d个按钮"%i)
+            self.input = wx.TextCtrl(self,-1,name="第%d个网络"%i)
+            self.input.SetValue(u'在此输入总预算')
+            self.btn.Bind(wx.EVT_BUTTON, lambda evt,mark=i,buuttnn=self.btn:self.changeWhichWeb(evt,mark,buuttnn))
+            sizer1 = wx.BoxSizer(wx.HORIZONTAL)
+            sizer1.Add(self.griddetail, 0, wx.EXPAND)
+            sizer1.Add(self.input, 0, wx.EXPAND)
+            sizer1.Add(self.btn, 0, wx.EXPAND)
+            sizer.Add(sizer1,0,wx.CENTER)
+
+        self.SetSizer(sizer)
+
+
+    def changeWhichWeb(self,event,mark,buuttnn):
+        print mark
+        self.whichInput2Show = wx.FindWindowByName('第%d个网络'%mark)
+        self.getCahngeValue = self.whichInput2Show.GetValue()
+
+        #用户改变批准条件后同步到数据库
+        db = MySQLdb.connect("localhost","root","punkisdead","BIMS", charset = "utf8")
+        cursor = db.cursor()
+        sql = "UPDATE WebBudget SET Ratified = '%s' WHERE WebName = '%s'" % (self.getCahngeValue,self.result[mark][1])
+        try:
+           cursor.execute(sql)
+           db.commit()
+        except:
+           db.rollback()
+        db.close()
 
 class frame_depart(wx.Frame):
     def __init__(self):
@@ -253,6 +304,7 @@ class frame_depart(wx.Frame):
         self.Bind(wx.EVT_MENU,self.writein, self.bumen_writein)
         self.Bind(wx.EVT_MENU,self.search, self.bumem_search)
         self.Bind(wx.EVT_MENU,self.ratifyitemmoney, self.uper_givemoney)
+        self.Bind(wx.EVT_MENU,self.ratifyWebMoney, self.uper_ratifyWebMoney)
 
         # self.Bind(wx.EVT_MENU,self.search, self.bumem_search)
         #输入页下拉框以及按钮的事件绑定
@@ -262,18 +314,43 @@ class frame_depart(wx.Frame):
         #项目搜索页的事件绑定
         self.panelsearch.bt.Bind(wx.EVT_BUTTON,self.submitsearch)
 
-    def writein(self , event):
-        self.panelsearch.Hide()
+    def hideAllPanel(self):
+        try:
+            self.panelshowsearchresult.Hide()
+        except:
+            pass
         try:
             self.panelMoneyTable.Hide()
         except:
             pass
+        try:
+            self.paneldetail.Hide()
+        except:
+            pass
+        try:
+            self.panelRatifiWebMoney.Hide()
+        except:
+            pass
+        try:
+            self.panelwritein.Hide()
+        except:
+            pass
+        try:
+            self.panelsearch.Hide()
+        except:
+            pass
+
+
+    def writein(self , event):
+        self.hideAllPanel()
         self.panelwritein.Show()
         self.Layout()
 
     def choice4money(self,event):
         self.whethermoney = self.panelwritein.choice_2.GetStringSelection()
         if self.whethermoney == u"否":        #出错提醒(故加u):Unicode equal comparison failed to convert both arguments to Unicode - interpreting them as being unequal
+            self.panelwritein.input_5.SetValue("")
+            self.panelwritein.input_6.SetValue("")
             self.panelwritein.input_5.Enable( False )
             self.panelwritein.input_6.Enable( False )
 
@@ -293,7 +370,6 @@ class frame_depart(wx.Frame):
         self.mainbroken = self.panelwritein.input_3_mainbroken.GetValue()
         self.askmoney = self.panelwritein.input_6.GetValue()
         self.reason4askmoney = self.panelwritein.input_5.GetValue()
-
 
         print type(self.askmoney),self.reason4askmoney,self.bridgename,type(self.detecttime),self.detecttype,self.parentWeb
         if self.bridgename=="" or self.detecttime=="" or self.detecttype == u"" or self.parentWeb=="":
@@ -417,11 +493,7 @@ class frame_depart(wx.Frame):
     def search(self,event):
         # self.dialogue_info = wx.Dialog(self,-1,"提示框",size = (300,150),pos=(600,300))
         # self.dialogue_info.Show()
-        try:
-            self.panelMoneyTable.Hide()
-        except:
-            pass
-        self.panelwritein.Hide()
+        self.hideAllPanel()
         self.panelsearch.Show()
         self.Layout()
 
@@ -512,10 +584,7 @@ class frame_depart(wx.Frame):
 
     def ratifyitemmoney(self,event):
         #此地有个问题待解决的是如果是从别的界面跳转到这儿，需要先关闭先前页面
-        try:
-            self.panelMoneyTable.Hide()
-        except:
-            pass
+        self.hideAllPanel()
         search = "select * from bridgeinfo WHERE WhetherDeclare = '%s' ORDER BY DetectTime"%u"是"
         self.result_money = exe(search)
         self.panelMoneyTable = panel_MoneyTable(self,self.result_money)
@@ -523,6 +592,15 @@ class frame_depart(wx.Frame):
         self.panelsearch.Hide()
         self.panelwritein.Hide()
         self.panelMoneyTable.Show()
+        self.Layout()
+
+    def ratifyWebMoney(self,event):
+        self.hideAllPanel()
+        self.panelRatifiWebMoney = panel_RatifyWebMoney(self)
+        self.sizer.Add(self.panelRatifiWebMoney, 1 , wx.EXPAND)
+        self.panelsearch.Hide()
+        self.panelwritein.Hide()
+        self.panelRatifiWebMoney.Show()
         self.Layout()
 
 app = wx.App()
